b1f7c800afe6de3f4207030dfe7e8dc2de177f28
김진현 <wlsgus63@naver.com>
2022-03-26 02:03:44 +0900

    Redux 모듈 예제 불러오기

--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -1,1 +1,56 @@
-"commit test"
+// widgets.js
+
+// Actions
+const LOAD = 'my-app/widgets/LOAD';
+const CREATE = 'my-app/widgets/CREATE';
+const UPDATE = 'my-app/widgets/UPDATE';
+const REMOVE = 'my-app/widgets/REMOVE';
+
+// Reducer
+export default function reducer(state = {}, action = {}) {
+    switch (action.type) {
+        // do reducer stuff
+        // 타입별로 어떤케이스를 정해주면 되겠구나..
+        default:
+            return state;
+    }
+}
+
+// Action Creators
+export function loadWidgets() {
+    return {
+        type: LOAD
+        
+    };
+}
+
+export function createWidget(widget) {
+    return {
+        type: CREATE,
+        
+        widget
+    };
+    //return { type : CREATE,widjet}
+        // {widjet:widjet} = > {widjet}
+}
+
+export function updateWidget(widget) {
+    return {
+        type: UPDATE,
+        widget
+    };
+}
+
+export function removeWidget(widget) {
+    return {
+        type: REMOVE,
+        widget
+    };
+}
+
+// side effects, only as applicable
+// e.g. thunks, epics, etc
+// export function getWidget() {
+//     return dispatch => get('/widget').then(widget => dispatch(updateWidget(widget)))
+// }
+//middleware입니다~
